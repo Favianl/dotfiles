75de4ceea9e239fb704d7e3d85a3802487c294b5
favianl <leofavianf@gmail.com>
2023-03-16 10:31:40 -0500

    install_apps edited

--- install_apps.sh
+++ install_apps.sh
@@ -13,9 +13,10 @@ rm -rf yay-bin
 
 yay -S ttf-mononoki-nerd brave-bin --noconfirm
 
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
 sleep 3
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
+exit
