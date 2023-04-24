69a6c2c51ba7289437949e34302ac861184ffa49
favianl <leofavianf@gmail.com>
2023-04-24 14:35:03 -0500

    kitty conf. added

--- config_links.sh
+++ config_links.sh
@@ -3,6 +3,9 @@
 rm -rf ~/.config/alacritty
 ln -s $HOME/dotfiles/.config/alacritty ~/.config/alacritty
 
+rm -rf ~/.config/kitty
+ln -s $HOME/dotfiles/.config/kitty ~/.config/kitty
+
 rm -rf ~/.config/nvim
 ln -s $HOME/dotfiles/.config/nvim ~/.config/nvim
 
