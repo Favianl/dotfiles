4675d717f2e3dfb3f6c6c93cc7f71c84d5f5d661
Favianl <leofavianl@gmail.com>
2023-07-14 17:31:08 -0500

    hyprland conf. added

---
+++ hyprland-conf/install.sh
@@ -0,0 +1,6 @@
+# !/bin/bash
+
+sudo pacman -S python3 neovim kitty firefox lsd bat base-devel vlc ttf-jetbrains-mono-nerd neofetch htop hyprland waybar wofi swappy grim slurp nemo nemo-fileroller polkit-gnome pamixer pavucontrol network-manager-applet gvfs lxappearance --noconfirm
+
+yay -S xdg-desktop-portal-hyprland-git --noconfirm
+
---
+++ qemu/install.sh
@@ -0,0 +1,3 @@
+# !/bin/bash
+
+sudo pacman -S qemu-full libvirt virt-manager iptables dnsmasq dmidecode bridge-utils openbsd-netcat --noconfirm
